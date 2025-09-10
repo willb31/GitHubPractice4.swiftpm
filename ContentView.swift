18cded9102f9c171d9afd5a04ba36fa1979da881
aprajapati50 <aprajapati7330@stu.d214.org>
2025-09-10 10:51:05 -0500

    made the task array to be of type task and changed the list to show the name of task property

--- ContentView.swift
+++ ContentView.swift
@@ -1,10 +1,14 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var tasks = ["Mow the Lawn, Feed the Dog, Grocery Shop"]
+    @State var tasks = [
+    Task(nameOfTask: "Mow The Lawn"),
+    Task(nameOfTask: "Feed the Dog"),
+    Task(nameOfTask: "Grocery Shop")
+]
     var body: some View {
         List(tasks, id: \.self) { task in
-        Text(task)
+        Text(task.nameOfTask)
         }
     
         
